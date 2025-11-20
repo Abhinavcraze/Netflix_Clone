0bb0d16229823f2ccf458e94eb3c8a5e36ac5779
Abhinav <abhinavbaskaran0803@gmail.com>
2025-11-20 15:49:10 +0530

    Commit

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,19 +3,22 @@ import './Login.css'
 import logo from '../../assets/logo.png'
 
 const Login = () => {
+
+  const [signState, setSignState] = React.useState(true); //true for sign up, false for sign in
+  
   return (
     <div className = 'login'>
       <img src={logo} className='login-logo' alt="" />
       <div className="login-container">
-        <h1>Sign Up</h1>
+        <h1>{signState ? "Sign Up" : "Sign In"}</h1>
         <form>
-          <label htmlFor="name">Name</label>
-          <input type="text" placeholder='Your Name...' />
+          {signState === true ? <label htmlFor="name">Name</label> : null}
+          {signState === true ?  <input type="text" placeholder='Your Name...' /> : null}
           <label htmlFor="email">Email</label>
           <input type="email" placeholder='Your Email...' />
           <label htmlFor="password">Password</label>
           <input type="password" placeholder='Your Password...' />
-          <button type='submit' className='login-btn'>Sign Up</button>
+          <button type='submit' className='login-btn'>{signState ? "Sign Up" : "Sign In"}</button>
 
           <div className="form-help">
             <div className="remember">
@@ -25,6 +28,12 @@ const Login = () => {
             <p>Need Help?</p>
           </div>
         </form>
+        <div className="form-switch">
+          {signState === false ? <p>New to Netflix?<span onClick = {() => setSignState(true)}>Sign Up Now</span></p> 
+              : <p>Already have an account?<span onClick = {() => setSignState(false)}>Sign In</span></p>}
+          
+          
+        </div>
       </div>
     </div>
   )
